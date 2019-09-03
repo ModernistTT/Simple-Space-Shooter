8d14e06c6e4f6f7613a27270793509903bda4c99
Aidan "Lucky" Somerville <49950736+ModernistTT@users.noreply.github.com>
2019-09-03 19:42:21 -0400

    Update EnemySpawner.cs

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 	
+	//The enemy
 	public GameObject enemy;
-    float randx;
+        //A float value that will be randomly created on a range of -16.16 to 1
+	float randx;
+	//This will be the position our enemies will be created at. The randomly generated value from the float above will be assigned to this position, making the enemies spawn in a random spot
 	Vector2 spawnPlace;
+	//How often our enemies will appear
 	public float spawnRate = 2f;
+	//The time before we create another enemy
 	float nextSpawn = 0f;
 	
 	void Start () 
@@ -19,7 +24,8 @@ public class EnemySpawner : MonoBehaviour {
 	{
 	    if (Time.time > nextSpawn)	
 	    {
-			nextSpawn = Time.time + spawnRate;
+			//Creates an enemy on a random position, and resets the time before creating another enemy
+		        nextSpawn = Time.time + spawnRate;
 			randx = Random.Range(-16.16f, 1f);
 			spawnPlace = new Vector2 (randx, transform.position.y);
 			Instantiate (enemy, spawnPlace, Quaternion.identity);

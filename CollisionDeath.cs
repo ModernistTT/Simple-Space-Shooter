a7655ff9fc3d977e211866f8d1644f163fb33cdc
Aidan "Lucky" Somerville <49950736+ModernistTT@users.noreply.github.com>
2019-09-03 19:19:42 -0400

    Update CollisionDeath.cs

--- CollisionDeath.cs
+++ CollisionDeath.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CollisionDeath : MonoBehaviour {
 	
+	//A Particle system (or particle effect) is a effect that can be used to create explosions, water from a waterfall, rain & more.
+	//A particle system takes a special type texture, and can duplicate that texture in to 3D "particles"
 	public ParticleSystem deathEffect;
+	//Our player
 	public GameObject player;
 
 	void Start () 
@@ -17,11 +20,14 @@ public class CollisionDeath : MonoBehaviour {
 	{
 	}
 	
+	
 	void OnTriggerEnter2D() 
 	{
+		//Destroys the player GameObject, moves the explosion effect to the player's position, 
 		player.SetActive(false);
 		deathEffect.transform.position = player.transform.position;
 		
+		//If a particle effect is assigned, the effect will render, however if an effect is not assigned, instead of creating an error, it instead ignores this line
 		if (deathEffect != null)
 		{
 			deathEffect.Play(); 

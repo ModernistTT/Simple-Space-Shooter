afe56a635cd7de30586e7cebb04ed45da038aec3
Aidan "Lucky" Somerville <49950736+ModernistTT@users.noreply.github.com>
2019-09-03 19:50:53 -0400

    Update Target.cs

--- Target.cs
+++ Target.cs
@@ -2,20 +2,24 @@
 
 public class Target : MonoBehaviour {
 
+    //This script allows the player to be able to shoot the enemies (Unfortunately I was unable to recover the gun script)
+    //The amount of health the enemy has
     public float health = 50f;
 
- //what makes the damage work
+    //If the health is below 0, destroy this object
     public void TakeDamage (float amount)
     {
         health -= amount;
          if (health <= 0f)            
         {
+            //Calls the "death" function
             Die();            
         }
     }
     
     void Die ()
     {
+        //Destroys the enemy
         Destroy(gameObject);
     }
 }

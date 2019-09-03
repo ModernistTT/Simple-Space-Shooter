1a2c233273c2fe200e8650672e1822350d1c1435
Aidan "Lucky" Somerville <49950736+ModernistTT@users.noreply.github.com>
2019-09-03 19:47:59 -0400

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -6,15 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
-  
+  //This script needs more context than anything, so heres what happenens in unity.
+  //Step 1: Create Main Menu Texture
+  //Step 2: Create button to play game (Unity has a built in button to just drag & drop)
+  //Step 3: Create button to quit game
+  //Step 4: Assign this script to the main menu
   public void PlayGame ()
   {
+          //This script does not recieve input from pressing the button, but recieves input from another script and then takes the input and uses it to load the game
 	  SceneManager.LoadScene("Game1");
 	  
   }  
 
   public void Quit ()
   {
+          //Terminates the application
 	  Application.Quit();
   }  
 
